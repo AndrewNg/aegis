e18477d7d655ac64b6d2f90337e55de503945f72
AndrewNg <andrew@chesscademy.com>
2014-04-18 23:28:23 -0400

    cleaned up extraneous code that was used for client side twilio

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  // Create an object which generates a capability token
-  var capability = new twilio.Capability(
-    "ACf790f48ac9a0c4a1cb5e5548945e0889",
-    "8be80276be5dd74cf822b080068b1fd4"
-  );
-
-  // outgoing
-  capability.allowClientOutgoing('APc42e53f2d09e115863fcdce98184bbb7');
-
-  // Render HTML page which contains capability token
+  // Render HTML page
   res.render('index', {
-    token:capability.generate(),
     title: 'aegis'
   });
 });
