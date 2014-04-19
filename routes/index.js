7ff02c6593ed2ff8cbeb4054dc7fc7ba354d3615
AndrewNg <andrew@chesscademy.com>
2014-04-18 21:27:03 -0400

    calling is set up. information that needs to be passed are the phone numbers and the movement event

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var twilio = require('twilio');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'lock' });
+  // Create an object which generates a capability token
+  var capability = new twilio.Capability(
+    "ACf790f48ac9a0c4a1cb5e5548945e0889",
+    "8be80276be5dd74cf822b080068b1fd4"
+  );
+
+  // Give capability generator permission to accept incoming calls
+  capability.allowClientIncoming('kevin');
+
+  // outgoing
+  capability.allowClientOutgoing('AP168c7b4fccfe20acc03dd2646626d0d3');
+
+  // Render HTML page which contains capability token
+  res.render('index', {
+    token:capability.generate(),
+    title: 'aegis'
+  });
 });
 
 module.exports = router;
