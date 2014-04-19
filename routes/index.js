7c639f5dc49628779bacee9ea10be2f8ab0368fd
AndrewNg <andrew@chesscademy.com>
2014-04-19 05:16:59 -0400

    cleaned up code, fixed leap bug

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var twilio = require('twilio');
-var twilio = require('twilio');
 var router = express.Router();
 
 /* GET home page. */
@@ -8,17 +7,6 @@ router.get('/', function(req, res) {
   // Render HTML page
   res.render('index', {
     title: 'aegis'
-  });
-});
-
-/* GET users */
-router.get('/userlist', function(req, res) {
-  var db = req.db;
-  var collection = db.get('usercollection');
-  collection.find({},{},function(e, docs){
-    res.render('userlist', {
-      "userlist": docs
-    });
   });
 });
 
